4756566e2f68593e459e2404a53c0c28e5cce761
Jared Miller <jared@smell.flowers>
2025-04-01 17:13:46 -0400

    Add main

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed frontend/dist
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:  "gash",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 1},
+		OnStartup:        app.startup,
+		Bind: []any{
+			app,
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
